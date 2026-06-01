6bdb5d5382ff0325b24e5b5a5664f941c7e407a6
JaimeTorresVilllalobos <jaimetorresvillalobos@gmail.com>
2026-05-31 21:34:44 -0600

    Create acercade.php

---
+++ acercade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es-mx">
+<head>
+<meta charset="UTF-8">
+<title>Acerca de - Práctica 12</title>
+<link rel="stylesheet" href="css/estilos.css"/>
+<link rel="stylesheet" href="css/menu.css"/>
+<link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
+</head>
+<body>
+<nav>
+  <ul>
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="acercade.php" class="current">Acerca de</a></li>
+  </ul>
+</nav>
+
+<div id="acerca">
+  <h2>Acerca del proyecto</h2>
+  <p><b>Nombre del proyecto:</b> Practica 12</p>
+  <p><b>Nombre del estudiante:</b> Jaime Torres Villlalobos</p>
+  <p><b>Número de control:</b> 23091054</p>
+  <p><b>Materia y grupo:</b> Programacion web - Grupo XB</p>
+  <div class="foto">
+    <img src="images/foto.jpg" alt="Foto del estudiante" width="300">
+  </div>
+</div>
+</body>
+</html>
